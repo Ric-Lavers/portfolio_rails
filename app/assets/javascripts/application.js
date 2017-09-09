f07e2f5062b0fd054327bd41bafe6aac6e2906bc
Ric Lavers <riclavers@Rics-MacBook-Pro-2.local>
2017-09-09 15:09:14 +1000

    sat commit

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,18 +1,4 @@
-// This is a manifest file that'll be compiled into application.js, which will include all the files
-// listed below.
-//
-// Any JavaScript/Coffee file within this directory, lib/assets/javascripts, or any plugin's
-// vendor/assets/javascripts directory can be referenced here using a relative path.
-//
-// It's not advisable to add code directly here, but if you do, it'll appear at the bottom of the
-// compiled file. JavaScript code in this file should be added after the last require_* statement.
-//
-// Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
-// about supported directives.
-//
-//= require rails-ujs
-//= require turbolinks
-//= require_tree .
+
 
 $(document).ready(function(){
 
@@ -44,23 +30,64 @@ $(document).ready(function(){
   });
 });
 
-$( window ).resize(function() {
-  var h = $(document).height();
-  console.log("Page height is " + h);
+// $( window ).resize(function() {
+//   var h = $(document).height();
+//   // console.log("Page height is " + h);
+// });
+
+// var path = document.getElementById('word');
+// var data = path.getAttribute('points');
+//   console.log(data);
+  // //
+  // var svg = document.getElementById('svg');
+  // var point = svg.createSVGPoint();
+  // point.x = 10;
+  // point.y = 20;
+  // var polyline= document.getElementById('Layer_3');
+  // polyline.points.appendItem(point);
+// $( window ).resize(function() {
+//   var top = $("#btn-contact").offset().top + window.screenY;
+//   var left = $("#btn-contact").offset().left + window.screenX;
+//   var width = $("#btn-contact").width();
+//   var height = $("#btn-contact").height();
+//
+//   var middleX = left + (width/2);
+//   var middleY = top + height;
+//
+//   // console.log("top: " + top + "| left: " + left + "| width: " + width + "| X: " + middleX + "| Y: " + middleY);
+
+$(".main-feature .menu a[href^='#']").on('click', function(e) {
+   // prevent default anchor click behavior
+   console.log("MY MAN");
+   e.preventDefault();
+   $('#btn-contact').removeClass('focus');
+   // store hash
+   var hash = this.hash;
+
+   // animate
+   $('html, body').animate({
+       scrollTop: $(hash).offset().top
+     }, 4000, function(){
+
+       // when done, add hash to url
+       // (default click behaviour)
+       window.location.hash = hash;
+     });
+
 });
 
 //draw Contact Box on scroll
 $(window).scroll(function() {
     var scroll = $(window).scrollTop();
-    console.log("scroll is " + scroll);
-    if (scroll >= 450) {
+    // console.log("scroll is " + scroll);
+    if (scroll >= 110) {
       $('#Layer_3').removeClass('static').addClass('motion');
       $('#text_1').addClass('text1');
       $('#text_2').addClass('text2');
       $('#text_3').addClass('text3');
       $('#text_4').addClass('text4');
     }
-    if (scroll <= 350 && $('#Layer_3').hasClass('motion')) {
+    if (scroll <= 109 && $('#Layer_3').hasClass('motion')) {
       $('#Layer_3').removeClass('motion').addClass('unmotion');
       $('#text_1').removeClass('text1');
       $('#text_2').removeClass('text2');
